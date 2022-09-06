cd48d703156a824edfc525d3d8c89a5624cf321c
Anna <anna.gloriole@gmail.com>
2022-09-07 02:08:18 +0300

    task05

---
+++ task05/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("введите 3х значное число");
+string number = Console.ReadLine ();
+Console.WriteLine(number[1]);
+
+
+//Можно ли так?
+//int number = new Random().Next(99,1000);      попробовала ч.з рендом, но ругается что:
+//Console.WriteLine(number); - при попытке вывести [1] по индексу говорит о несоответствии int, string
+//пыталась эксперементировать, но ругается о невозможности void
+//string number1 = Console.ReadLine(Console.WriteLine(number));
+//Console.WriteLine(Console.WriteLine(number)[1]);
+
