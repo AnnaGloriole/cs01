a7c7840336015430e802ab2d51f25fb2a0930991
Anna <anna.gloriole@gmail.com>
2022-09-06 17:30:48 +0300

    task03

---
+++ task03/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("введите число");
+int num = int.Parse(Console.ReadLine());
+if (num == 0)
+{
+    Console.WriteLine("ноль");
+}
+else if (num % 2 == 0)
+{
+    Console.WriteLine("четное");
+}
+else
+{
+    Console.WriteLine("нечетное");
+}
