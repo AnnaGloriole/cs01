439b82286430733edcb1b8757e69c0067eb620c5
Anna <anna.gloriole@gmail.com>
2022-09-06 17:26:34 +0300

    task02

---
+++ task02/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите 3 числа");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int max = Max(a, b, c);
+Console.WriteLine(max);
