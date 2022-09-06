d20bad01aa97249f949ef80afd6828cb9fe13253
Anna <anna.gloriole@gmail.com>
2022-09-06 17:22:06 +0300

    task 01

---
+++ task01/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("введите 2 числа");
+double num1 = double.Parse(Console.ReadLine());
+double num2 = double.Parse(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine($"max={num1}, min={num2}");
+}
+else if (num2 > num1)
+{
+    Console.WriteLine($"max={num2}, min={num1}");
+}
+else
+{
+    Console.WriteLine("числа равны");
+}
